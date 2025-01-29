a16ae1a5ce4062a1e982f6020cf7198ce573472c
Samuel <samuelfelipelorente@gmail.com>
2025-01-29 22:51:04 +0100

    update dia 2 :)

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ function App() {
         const savedTodos = localStorage.getItem('todos');
         return savedTodos ? JSON.parse(savedTodos) : [];
       });
-      console.log(todos);
 
     const [types, setTypes] = useState(() => {
     // Cargar los types desde localStorage si están disponibles
